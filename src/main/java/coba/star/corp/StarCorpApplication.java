f7c6a4d3f9d109ddca6da9cc1eeb48c26a2d6f8a
Fadhilahlaras <fadhilahlaras97@gmail.com>
2023-09-04 13:40:21 +0700

    Rename spring application name

--- src/main/java/coba/star/corp/StarCorpApplication.java
+++ src/main/java/coba/star/corp/StarCorpApplication.java
@@ -1,14 +1,13 @@
-package coba.daily.you;
-
+package coba.star.corp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YouApplication {
+public class StarCorpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YouApplication.class, args);
+		SpringApplication.run(StarCorpApplication.class, args);
 	}
 
 }
